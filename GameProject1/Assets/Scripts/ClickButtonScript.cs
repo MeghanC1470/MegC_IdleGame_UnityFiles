1071b4e40003b754f7eb4733adf59460f5f4b40d
MeghanC1470 <meghanc2000@outlook.com>
2021-10-21 16:22:12 -0400

    MegC_Idle_ButtonRepair

---
+++ GameProject1/Assets/Scripts/ClickButtonScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickButtonScript : MonoBehaviour
+{
+    public GameObject textbox;
+
+    public void ClickTheButton ()
+    {
+        textbox.SetActive(true);
+    }
+
+
+}
